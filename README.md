e34bb6b582dcbdcedb1b288013a5b507a33d35ed
DaShaun <dashaun@redislabs.com>
2021-06-07 21:19:33 -0500

    first pass

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # io.dashaun.function.azure.helloworld
-Building an Azure Function with Microsoft OpenJDK 16.0.1 and using "local" azure functions for testing
+Building an Azure Function with Microsoft OpenJDK 16.0.1
+using "local" azure functions for testing
+
+This can't be deployed using Java 16, so I downgraded to Java 11.
+
+## Steps To Build
+I don't have Maven installed, so I used the latest version via a Docker image to build from the archetype and install the maven wrapper.
+```bash
+docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "$PWD/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven mvn archetype:generate -DarchetypeGroupId=com.microsoft.azure -DarchetypeArtifactId=azure-functions-archetype -DjavaVersion=16
+docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "$PWD/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven mvn -N io.takari:maven:wrapper
+```
+
+## Quickstart
+
+```bash
+./mvnw azure-functions:run
+```
+
+## Deploy
+
+You will need to create a unique function name before doing this.
+
+```bash
+./mvnw azure-functions:deploy
+```
+
+You will see something like this:
+```markdown
+[INFO] HTTP Trigger Urls:
+[INFO]   httpexample : https://dashaun-azure-helloworld.azurewebsites.net/api/httpexample
+```
---
+++ src/main/java/io/dashaun/function/azure/Function.java
@@ -0,0 +1,58 @@
+package io.dashaun.function.azure;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.AzureAuthorityHosts;
+import com.azure.identity.EnvironmentCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     */
+    @FunctionName("HttpExample")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                    name = "req",
+                    methods = {HttpMethod.GET, HttpMethod.POST},
+                    authLevel = AuthorizationLevel.ANONYMOUS)
+                    HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        try {
+            TokenCredential credential = new EnvironmentCredentialBuilder()
+                    .authorityHost(AzureAuthorityHosts.AZURE_PUBLIC_CLOUD)
+                    .build();
+            AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            AzureResourceManager.Authenticated azureResourceManager = AzureResourceManager.configure()
+                    .withLogLevel(HttpLogDetailLevel.BASIC)
+                    .authenticate(credential, profile);
+
+            return request.createResponseBuilder(HttpStatus.OK).body(azureResourceManager.tenantId()).build();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return request.createResponseBuilder(HttpStatus.OK).body(e.getMessage()).build();
+        }
+    }
+}
